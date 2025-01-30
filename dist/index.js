eaa689c979b547005d23c34fa3c434023089765d
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2025-01-30 09:16:48 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const httpserver = app.listen(3000);
 const wss = new ws_1.WebSocketServer({ server: httpserver });
+app.use((0, cors_1.default)());
 wss.on('connection', (ws) => {
     ws.on('error', console.error);
     //  console.log(wss.clients)
